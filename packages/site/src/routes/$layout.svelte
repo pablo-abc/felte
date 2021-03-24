c03d6fa9acc8aff62a70a6849100c6c9788812c9
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-03-24 17:26:41 +0100

    fix(site): fix safari bug

--- packages/site/src/routes/$layout.svelte
+++ packages/site/src/routes/$layout.svelte
@@ -2,6 +2,13 @@
   import Nav from '$lib/components/Nav.svelte';
   import 'tippy.js/dist/tippy.css';
   import 'highlight.js/styles/a11y-dark.css';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (location.hash) {
+      location.replace(`${location.pathname}${location.hash}`);
+    }
+  });
 </script>
 
 <Nav />
