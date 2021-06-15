1f3d8ae68cfc5989981de717d771b399702e8ba6
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-06-15 20:20:45 +0200

    feat(site): add html minifier

---
+++ packages/site/hooks.js
@@ -0,0 +1,31 @@
+import { minify } from 'html-minifier';
+import { prerendering } from '$app/env';
+
+const minification_options = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true,
+  decodeEntities: true,
+  html5: true,
+  ignoreCustomComments: [/^#/],
+  minifyCSS: true,
+  minifyJS: false,
+  removeAttributeQuotes: true,
+  removeComments: true,
+  removeOptionalTags: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  sortAttributes: true,
+  sortClassName: true,
+};
+
+export async function handle({ request, render }) {
+  const response = await render(request);
+
+  if (prerendering && response.headers['content-type'] === 'text/html') {
+    response.body = minify(response.body, minification_options);
+  }
+
+  return response;
+}
