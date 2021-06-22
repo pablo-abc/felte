3380d50340d84abbe47a878753e225b6621ddcb7
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-06-22 20:54:20 +0200

    build: update typescript rollup tooling

--- packages/common/rollup.config.js
+++ packages/common/rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@wessberg/rollup-plugin-ts';
+import typescript from 'rollup-plugin-ts';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
