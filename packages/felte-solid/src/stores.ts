7cd27feead2569a0d471b0206cf6ebe15421e60b
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-07-01 17:19:34 +0200

    feat(felte-solid): wip

--- packages/felte-solid/src/stores.ts
+++ packages/felte-solid/src/stores.ts
@@ -1,4 +1,3 @@
-import { derived, writable } from 'svelte/store';
 import {
   deepSet,
   deepSome,
@@ -7,8 +6,8 @@ import {
   _isPlainObject,
   _mergeWith,
 } from '@felte/common';
-import type { Errors, FormConfig, Touched, Stores } from '@felte/common';
-import { createEffect, createSignal } from 'solid-js';
+import type { Errors, FormConfig, Touched, Obj } from '@felte/common';
+import { createEffect, createSignal, createRoot } from 'solid-js';
 import { createStore, reconcile } from 'solid-js/store';
 
 export function createStores<Data extends Record<string, unknown>>(
@@ -18,27 +17,33 @@ export function createStores<Data extends Record<string, unknown>>(
     config.initialValues || ({} as Data),
     false
   );
+  const initialErrors: Errors<Data> = deepSet<Data, null>(
+    config.initialValues || ({} as Data),
+    null
+  );
 
   const [dataStore, setDataStore] = createStore<Data>(
     config.initialValues ? _cloneDeep(config.initialValues) : ({} as Data)
   );
+
   const [errorStore, setErrorStore] = createStore<Errors<Data>>(
     {} as Errors<Data>
   );
 
-  createEffect(() => {
-    async function validate($data?: Data) {
-      let errors: Errors<Data> | undefined = {};
-      if (!config.validate || !$data) return;
-      errors = await executeValidation($data, config.validate);
-      setErrorStore(reconcile((errors as any) || {}));
-    }
-    validate(dataStore);
-  });
+  const [resultErrors, setResultErrors] = createStore<Errors<Data>>(
+    initialErrors
+  );
 
   const [touchedStore, setTouchedStore] = createStore<Touched<Data>>(
     initialTouched
   );
+
+  async function validate($data?: Data) {
+    let errors: Errors<Data> | undefined = {};
+    if (!config.validate || !$data) return;
+    errors = await executeValidation($data, config.validate);
+    setErrorStore(reconcile((errors as any) || {}));
+  }
 
   function errorFilterer(
     errValue?: string | string[],
@@ -55,11 +60,22 @@ export function createStores<Data extends Record<string, unknown>>(
     return (touchValue && errValue) || null;
   }
 
-  const [resultErrors, setResultErrors] = createStore<Errors<Data>>(
-    _mergeWith<Errors<Data>>(errorStore, touchedStore, errorFilterer)
-  );
-
   const [isValidStore, setIsValidStore] = createSignal(!config.validate);
+
+  function createSubscriber<T extends Obj | boolean>(store: T | (() => T)) {
+    return function subscribe(fn: (data: T) => void) {
+      const value = typeof store === 'function' ? store() : store;
+      fn(value);
+      let disposer;
+      createRoot((dispose) => {
+        disposer = dispose;
+        createEffect(() => {
+          fn(value);
+        });
+      });
+      return disposer ?? (() => undefined);
+    };
+  }
 
   let firstCalled = false;
   createEffect(() => {
@@ -68,27 +84,26 @@ export function createStores<Data extends Record<string, unknown>>(
       firstCalled = true;
       return setIsValidStore(false);
     }
-    const hasErrors = deepSome(errorStore, (error) => !!error);
+    const hasErrors = deepSome(resultErrors, (error) => !!error);
     setIsValidStore(!hasErrors);
   });
 
   createEffect(() => {
-    setResultErrors(
-      reconcile(
-        _mergeWith<Errors<Data>>(errorStore, touchedStore, errorFilterer) as any
-      )
-    );
+    const errors = _mergeWith<Errors<Data>>(
+      errorStore,
+      touchedStore,
+      errorFilterer
+    ) as any;
+    setResultErrors(errors);
   });
 
   const [isSubmittingStore, setIsSubmittingStore] = createSignal(false);
 
-  function subscribeData(fn: (data: Data) => void) {
-    fn(dataStore);
-    createEffect(() => {
-      fn(dataStore);
-    });
-    return () => undefined;
-  }
+  createEffect(() => {
+    validate(dataStore);
+  });
+
+  const subscribeData = createSubscriber<Data>(dataStore);
 
   function setData(data: Data) {
     setDataStore(reconcile(data));
@@ -98,16 +113,12 @@ export function createStores<Data extends Record<string, unknown>>(
     setDataStore(reconcile(updater(_cloneDeep(dataStore))));
   }
 
-  function subscribeErrors(fn: (errors: Errors<Data>) => void) {
-    fn(resultErrors as Errors<Data>);
-    createEffect(() => {
-      fn(resultErrors as Errors<Data>);
-    });
-    return () => undefined;
-  }
+  const subscribeErrors = createSubscriber<Errors<Data>>(
+    resultErrors as Errors<Data>
+  );
 
   function setErrors(errors: Errors<Data>) {
-    setErrorStore(errors);
+    setErrorStore(reconcile(errors as any));
   }
 
   function updateErrors(updater: (data: Errors<Data>) => Errors<Data>) {
@@ -115,13 +126,7 @@ export function createStores<Data extends Record<string, unknown>>(
     setErrorStore(reconcile(errors));
   }
 
-  function subscribeTouched(fn: (touched: Touched<Data>) => void) {
-    fn(touchedStore);
-    createEffect(() => {
-      fn(touchedStore);
-    });
-    return () => undefined;
-  }
+  const subscribeTouched = createSubscriber<Touched<Data>>(touchedStore);
 
   function setTouched(touched: Touched<Data>) {
     setTouchedStore(reconcile(touched as any) as any);
@@ -133,13 +138,7 @@ export function createStores<Data extends Record<string, unknown>>(
     );
   }
 
-  function subscribeIsValid(fn: (data: boolean) => void) {
-    fn(isValidStore());
-    createEffect(() => {
-      fn(isValidStore());
-    });
-    return () => undefined;
-  }
+  const subscribeIsValid = createSubscriber<boolean>(isValidStore);
 
   function setIsValid(data: boolean) {
     setIsValidStore(data);
@@ -149,13 +148,7 @@ export function createStores<Data extends Record<string, unknown>>(
     setIsValidStore(updater(isValidStore()));
   }
 
-  function subscribeIsSubmitting(fn: (data: boolean) => void) {
-    fn(isSubmittingStore());
-    createEffect(() => {
-      fn(isValidStore());
-    });
-    return () => undefined;
-  }
+  const subscribeIsSubmitting = createSubscriber<boolean>(isSubmittingStore);
 
   function setIsSubmitting(data: boolean) {
     setIsSubmittingStore(data);
@@ -199,79 +192,4 @@ export function createStores<Data extends Record<string, unknown>>(
   };
 
   return stores;
-}
-
-export function createSvelteStores<Data extends Record<string, unknown>>(
-  config: FormConfig<Data>
-): Stores<Data> {
-  const initialTouched: Touched<Data> = deepSet<Data, boolean>(
-    config.initialValues || ({} as Data),
-    false
-  );
-
-  const touched = writable(initialTouched);
-
-  const data = writable(
-    config.initialValues ? _cloneDeep(config.initialValues) : ({} as Data)
-  );
-
-  const errors = writable(
-    {} as Errors<Data>,
-    (set: (values: Errors<Data>) => void) => {
-      async function validate($data?: Data) {
-        let errors: Errors<Data> | undefined = {};
-        if (!config.validate || !$data) return;
-        errors = await executeValidation($data, config.validate);
-        set(errors || {});
-      }
-      return data.subscribe(validate);
-    }
-  );
-
-  function errorFilterer(
-    errValue?: string | string[],
-    touchValue?: boolean | boolean[]
-  ) {
-    if (_isPlainObject(touchValue)) return;
-    if (Array.isArray(touchValue)) {
-      if (touchValue.some(_isPlainObject)) return;
-      const errArray = Array.isArray(errValue) ? errValue : [];
-      return touchValue.map(
-        (value, index) => (value && errArray[index]) || null
-      );
-    }
-    return (touchValue && errValue) || null;
-  }
-
-  const { subscribe: errorSubscribe } = derived(
-    [errors, touched],
-    ([$errors, $touched]) => {
-      return _mergeWith<Errors<Data>>($errors, $touched, errorFilterer);
-    }
-  );
-
-  let firstCalled = false;
-  const isValid = derived(errors, ($errors) => {
-    if (!config.validate) return true;
-    if (!firstCalled) {
-      firstCalled = true;
-      return false;
-    }
-    const hasErrors = deepSome($errors, (error) => !!error);
-    return !hasErrors;
-  });
-
-  const isSubmitting = writable(false);
-
-  return {
-    touched,
-    isSubmitting,
-    isValid,
-    errors: {
-      subscribe: errorSubscribe,
-      set: errors.set,
-      update: errors.update,
-    },
-    data,
-  };
 }
