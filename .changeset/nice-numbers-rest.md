14bf9d84da94ec955056211d3923af7183280da0
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-07-20 21:40:21 +0200

    refactor: use only features compatible with Bundlephobia

---
+++ .changeset/nice-numbers-rest.md
@@ -0,0 +1,6 @@
+---
+'@felte/reporter-solid': patch
+'@felte/reporter-svelte': patch
+---
+
+Update to use only JS features compatible with Bundlephobia
--- packages/reporter-solid/rollup.config.js
+++ packages/reporter-solid/rollup.config.js
@@ -1,24 +1,64 @@
+import typescript from 'rollup-plugin-ts';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import jsxPlugin from '@solid-reach/rollup-plugin-jsx';
+import jsx from 'acorn-jsx';
+import bundleSize from 'rollup-plugin-bundle-size';
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'es',
-    },
-  ],
-  external: ['solid-js', 'solid-js/web'],
-  plugins: [
-    nodeResolve({
-      extensions: ['.js', '.ts', '.tsx'],
-    }),
-    babel({
-      extensions: ['.js', '.ts', '.tsx'],
-      babelHelpers: 'bundled',
-      presets: ['solid', '@babel/preset-typescript'],
-      exclude: 'node_modules/**',
-    }),
-  ],
-};
+export default [
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: 'dist/index.jsx',
+        format: 'es',
+      },
+    ],
+    external: ['solid-js', 'solid-js/web', 'solid-js/store'],
+    acornInjectPlugins: [jsx()],
+    plugins: [
+      nodeResolve({
+        extensions: ['.js', '.ts', '.tsx'],
+      }),
+      typescript(),
+      babel({
+        extensions: ['.js', '.ts', '.tsx'],
+        babelHelpers: 'bundled',
+        plugins: [
+          '@babel/plugin-syntax-jsx',
+          'babel-plugin-annotate-pure-calls',
+          'babel-plugin-dev-expression',
+        ],
+        exclude: 'node_modules/**',
+      }),
+      jsxPlugin(),
+    ],
+  },
+  {
+    input: 'src/index.tsx',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'es',
+      },
+    ],
+    external: ['solid-js', 'solid-js/web', 'solid-js/store'],
+    plugins: [
+      nodeResolve({
+        extensions: ['.js', '.ts', '.tsx'],
+      }),
+      typescript(),
+      babel({
+        extensions: ['.js', '.ts', '.tsx'],
+        babelHelpers: 'bundled',
+        presets: ['solid'],
+        plugins: [
+          'babel-plugin-annotate-pure-calls',
+          'babel-plugin-dev-expression',
+        ],
+        exclude: 'node_modules/**',
+      }),
+      bundleSize(),
+    ],
+  },
+];
