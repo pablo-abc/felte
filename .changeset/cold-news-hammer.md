0bce41e3f19f1e3a80d6edfd018955182d6bbbd1
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-08-02 12:00:24 +0200

    fix: fix validator rollup config

---
+++ .changeset/cold-news-hammer.md
@@ -0,0 +1,7 @@
+---
+'@felte/validator-superstruct': patch
+'@felte/validator-yup': patch
+'@felte/validator-zod': patch
+---
+
+Fix rollup config for validators
--- packages/validator-zod/rollup.config.js
+++ packages/validator-zod/rollup.config.js
@@ -14,7 +14,7 @@ const name = pkg.name
 
 export default {
   input: './src/index.ts',
-  external: ['tippy.js'],
+  external: ['zod'],
   output: [
     {
       file: pkg.browser,
