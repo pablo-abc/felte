b8c4a9efcc3aeabec28b5c510a4ab59d761a8422
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-01-12 21:59:34 +0100

    feat(site): add v1 banner

--- packages/site/src/routes/__layout.svelte
+++ packages/site/src/routes/__layout.svelte
@@ -4,8 +4,47 @@
   import Nav from '$lib/components/Nav.svelte';
   import 'tippy.js/dist/tippy.css';
   import 'highlight.js/styles/a11y-dark.css';
+
+  const currentVersion = import.meta.env.VITE_FELTE_VERSION;
+  const altVersion = import.meta.env.VITE_FELTE_ALT_VERSION;
+  const altLink = import.meta.env.VITE_FELTE_ALT_LINK;
 </script>
 
+<div class="banner mobile">
+  If you're looking for the documentation for {altVersion}, <a href="{altLink}" re="external">click here</a>.
+</div>
+<div class="banner desktop">
+  This is the documentation for Felte {currentVersion}. If you're looking for the documentation for {altVersion}, <a href="{altLink}" re="external">click here</a>.
+</div>
 <Nav />
 
 <slot></slot>
+
+<style>
+  .banner {
+    background-color: var(--primary-color);
+    color: #FFF;
+    font-size: 0.9rem;
+    font-weight: 700;
+    padding: 0.5rem;
+    text-align: center;
+  }
+
+  .banner a {
+    color: #FFF;
+  }
+
+  .desktop {
+    display: none;
+  }
+
+  @media only screen and (min-width: 900px) {
+    .desktop {
+      display: block;
+    }
+
+    .mobile {
+      display: none;
+    }
+  }
+</style>
