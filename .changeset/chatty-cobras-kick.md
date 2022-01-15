8c29b4a87d74c317a9b959a2afe0ca67c9f49ad5
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-01-15 23:22:29 +0100

    fix(common): fix unset on Safari

---
+++ .changeset/chatty-cobras-kick.md
@@ -0,0 +1,10 @@
+---
+'@felte/common': patch
+'@felte/core': patch
+'@felte/extender-persist': patch
+'felte': patch
+'@felte/react': patch
+'@felte/solid': patch
+---
+
+Fix unset on Safari
--- packages/common/src/utils/unset.ts
+++ packages/common/src/utils/unset.ts
@@ -1,5 +1,6 @@
 import type { Obj } from '../types';
 import { _cloneDeep } from './cloneDeep';
+import { _get } from './get';
 
 /** @ignore */
 export function _unset(obj: undefined, path: string | string[]): undefined;
@@ -11,25 +12,18 @@ export function _unset<Data extends Obj>(
   obj: Data | undefined,
   path: string | string[]
 ): Data | undefined {
-  if (Object(obj) !== obj) return;
+  if (!obj || Object(obj) !== obj) return;
   // When obj is not an object
   else if (typeof obj !== 'undefined') obj = _cloneDeep<Data>(obj);
   // If not yet an array, get the keys from the string-path
   const newPath = !Array.isArray(path)
     ? path.toString().match(/[^.[\]]+/g) || []
     : path;
-  const foundProp = newPath.slice(0, -1).reduce(
-    (a: any, c: any) =>
-      Object(a[c]) === a[c] // Does the key exist and is its value an object?
-        ? // Yes: then follow that path
-          a[c]
-        : undefined,
-    obj
-  );
+  const foundProp: any = _get(obj, newPath.slice(0, -1).join('.'));
   if (Array.isArray(foundProp)) {
     foundProp.splice(Number(newPath[newPath.length - 1]), 1);
   } else {
     delete foundProp?.[newPath[newPath.length - 1]];
   }
-  return obj as Data; // Return the top-level object to allow chaining
+  return obj as Data;
 }
