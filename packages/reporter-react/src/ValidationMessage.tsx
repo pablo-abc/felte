a2d39e15ea2f54982fc50f72c508221441ea5c92
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-01-17 03:26:06 +0100

    fix(reporter-react): use setTimeout to guarantee DOM has rendered

--- packages/reporter-react/src/ValidationMessage.tsx
+++ packages/reporter-react/src/ValidationMessage.tsx
@@ -1,6 +1,6 @@
 import type { Errors, Obj } from '@felte/common';
 import type { ReactNode } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { _get, getPath } from '@felte/common';
 import { errorStores, warningStores } from './stores';
 import { createId } from './utils';
@@ -17,32 +17,37 @@ export function ValidationMessage<Data extends Obj = Obj>(
 ) {
   const level = props.level ?? 'error';
   const [messages, setMessages] = useState<string | string[]>();
+  const id = useMemo(() => createId(21), []);
   function getFormElement(element: HTMLDivElement) {
     return element.closest('form');
   }
 
-  const id = createId(21);
   useEffect(() => {
-    const element = document.getElementById(id) as HTMLDivElement;
-    const path =
-      typeof props.index !== 'undefined'
-        ? `${props.for}[${props.index}]`
-        : props.for;
-    const errorPath = getPath(element, path);
-    const formElement = getFormElement(element) as HTMLFormElement;
-    const reporterId = formElement?.dataset.felteReporterReactId;
-    if (!reporterId) return;
-    if (level === 'error') {
-      const errors = errorStores[reporterId];
-      return errors.subscribe(($errors: Errors<Data>) => {
-        setMessages(_get($errors, errorPath) as any);
-      });
-    } else {
-      const warnings = warningStores[reporterId];
-      return warnings.subscribe(($warnings: Errors<Data>) => {
-        setMessages(_get($warnings, errorPath) as any);
-      });
-    }
+    let unsubscriber;
+    // To guarantee the DOM has rendered we need to setTimeout
+    setTimeout(() => {
+      const element = document.getElementById(id) as HTMLDivElement;
+      const path =
+        typeof props.index !== 'undefined'
+          ? `${props.for}[${props.index}]`
+          : props.for;
+      const errorPath = getPath(element, path);
+      const formElement = getFormElement(element) as HTMLFormElement;
+      const reporterId = formElement?.dataset.felteReporterReactId;
+      if (!reporterId) return;
+      if (level === 'error') {
+        const errors = errorStores[reporterId];
+        unsubscriber = errors.subscribe(($errors: Errors<Data>) => {
+          setMessages(_get($errors, errorPath) as any);
+        });
+      } else {
+        const warnings = warningStores[reporterId];
+        unsubscriber = warnings.subscribe(($warnings: Errors<Data>) => {
+          setMessages(_get($warnings, errorPath) as any);
+        });
+      }
+    });
+    return unsubscriber;
   }, []);
 
   return (
