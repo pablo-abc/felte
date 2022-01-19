c86a82ad3208d62aed37c41d29665897d5811cd8
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-01-19 14:58:19 +0100

    chore: preserve modules in cjs

--- packages/common/rollup.config.js
+++ packages/common/rollup.config.js
@@ -2,7 +2,6 @@ import typescript from 'rollup-plugin-ts';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import bundleSize from 'rollup-plugin-bundle-size';
 import pkg from './package.json';
 
 const prod = process.env.NODE_ENV === 'production';
@@ -11,52 +10,36 @@ const name = pkg.name
   .replace(/^\w/, (m) => m.toUpperCase())
   .replace(/-\w/g, (m) => m[1].toUpperCase());
 
-export default [
-  {
-    input: './src/index.ts',
-    output: {
-      file: pkg.browser,
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      dir: 'dist/cjs',
       format: 'cjs',
       sourcemap: prod,
       exports: 'named',
       name,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
     },
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(
-          prod ? 'production' : 'development'
-        ),
-        preventAssignment: true,
-      }),
-      resolve({ browser: true }),
-      commonjs(),
-      typescript({ browserslist: false }),
-
-      prod && bundleSize(),
-    ],
-  },
-  {
-    input: ['./src/index.ts'],
-    output: {
+    {
       dir: 'dist/esm',
       format: 'esm',
       sourcemap: prod,
       exports: 'named',
       preserveModules: true,
+      preserveModulesRoot: 'src',
     },
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(
-          prod ? 'production' : 'development'
-        ),
-        preventAssignment: true,
-      }),
-      resolve({ browser: true }),
-      commonjs(),
-      typescript({
-        declarationDir: './dist/esm',
-        browserslist: false,
-      }),
-    ],
-  },
-];
+  ],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        prod ? 'production' : 'development'
+      ),
+      preventAssignment: true,
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    typescript({ browserslist: false }),
+  ],
+};
