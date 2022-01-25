df4b7b9d9a388611fbb3e5407ddacb29ab2d4d29
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-01-25 21:08:29 +0100

    test(core): update tests

---
+++ packages/core/tests/stores.test.ts
@@ -0,0 +1,69 @@
+import { waitFor } from '@testing-library/dom';
+import { createStores } from '../src/stores';
+import { writable, get } from 'svelte/store';
+
+describe('createStores', () => {
+  test('filters errors', async () => {
+    const { start, cleanup, errors, touched, warnings } = createStores(
+      writable,
+      {
+        initialValues: {
+          arr: [
+            {
+              value: '',
+            },
+          ],
+          strings: ['test'],
+        },
+        preventStoreStart: true,
+      }
+    );
+    const clean = start();
+    await waitFor(() => {
+      expect(get(errors)).toEqual({
+        arr: [
+          {
+            value: null,
+          },
+        ],
+        strings: [null],
+      });
+      expect(get(warnings)).toEqual({
+        arr: [
+          {
+            value: null,
+          },
+        ],
+        strings: [null],
+      });
+    });
+    errors.set({
+      arr: [
+        {
+          value: 'test error',
+        },
+      ],
+      strings: [],
+    });
+    touched.set({
+      arr: [
+        {
+          value: true,
+        },
+      ],
+      strings: true,
+    });
+    await waitFor(() => {
+      expect(get(errors)).toEqual({
+        arr: [
+          {
+            value: ['test error'],
+          },
+        ],
+        strings: null,
+      });
+    });
+    cleanup();
+    clean();
+  });
+});
