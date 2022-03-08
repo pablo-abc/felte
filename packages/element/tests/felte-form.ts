5dd7e2a256e3d20b28882749fb7bff40a3cb5183
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-08 04:50:58 +0100

    fix(element): comment unused code

--- packages/element/tests/felte-form.ts
+++ packages/element/tests/felte-form.ts
@@ -1,15 +1,15 @@
-import * as sinon from 'sinon';
 import { suite } from 'uvu';
-import { expect } from 'uvu-expect';
-import userEvent from '@testing-library/user-event';
-import { screen, waitFor } from '@testing-library/dom';
-import {
-  createDOM,
-  cleanupDOM,
-  createInputElement,
-  createMultipleInputElements,
-  createForm,
-} from './common';
+// import * as sinon from 'sinon';
+// import { expect } from 'uvu-expect';
+// import userEvent from '@testing-library/user-event';
+// import { screen, waitFor } from '@testing-library/dom';
+// import {
+//   createDOM,
+//   cleanupDOM,
+//   createInputElement,
+//   createMultipleInputElements,
+//   createForm,
+// } from './common';
 
 const FelteForm = suite('FelteForm');
 
