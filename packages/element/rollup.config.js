531bf96a3b05751f5aa95063d482d1c87ce21039
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-10 14:46:12 +0100

    build(element): add separate builds for felte-form and felte-field

--- packages/element/rollup.config.js
+++ packages/element/rollup.config.js
@@ -4,12 +4,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import bundleSize from 'rollup-plugin-bundle-size';
 import { terser } from 'rollup-plugin-terser';
-import renameNodeModules from 'rollup-plugin-rename-node-modules';
 import pkg from './package.json';
 
 const prod = !process.env.ROLLUP_WATCH;
 
-export default [
+const builds = [
   {
     input: './src/index.ts',
     output: [
@@ -25,15 +24,13 @@ export default [
         format: 'esm',
         sourcemap: prod,
         exports: 'named',
-        plugins: [terser(), bundleSize()],
+        plugins: [terser()],
       },
       {
-        dir: 'dist/esm',
+        file: pkg.module,
         format: 'esm',
         sourcemap: prod,
         exports: 'named',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
       },
     ],
     plugins: [
@@ -46,7 +43,87 @@ export default [
       resolve({ browser: true, exportConditions: prod ? [] : ['development'] }),
       commonjs(),
       typescript({ browserslist: false }),
-      renameNodeModules('external', prod),
+      prod && bundleSize(),
     ],
   },
 ];
+
+if (prod) {
+  builds.push({
+    input: './src/felte-form.ts',
+    output: [
+      {
+        file: 'dist/felte-form.cjs',
+        format: 'umd',
+        sourcemap: prod,
+        exports: 'named',
+        name: 'FelteReporterElement',
+      },
+      {
+        file: 'dist/esm/felte-form.min.js',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+        plugins: [terser()],
+      },
+      {
+        file: 'dist/esm/felte-form.js',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(
+          prod ? 'production' : 'development'
+        ),
+        preventAssignment: true,
+      }),
+      resolve({ browser: true, exportConditions: prod ? [] : ['development'] }),
+      commonjs(),
+      typescript({ browserslist: false }),
+      prod && bundleSize(),
+    ],
+  });
+
+  builds.push({
+    input: './src/felte-field.ts',
+    output: [
+      {
+        file: 'dist/felte-field.cjs',
+        format: 'umd',
+        sourcemap: prod,
+        exports: 'named',
+        name: 'FelteReporterElement',
+      },
+      {
+        file: 'dist/esm/felte-field.min.js',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+        plugins: [terser()],
+      },
+      {
+        file: 'dist/esm/felte-field.js',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(
+          prod ? 'production' : 'development'
+        ),
+        preventAssignment: true,
+      }),
+      resolve({ browser: true, exportConditions: prod ? [] : ['development'] }),
+      commonjs(),
+      typescript({ browserslist: false }),
+      prod && bundleSize(),
+    ],
+  });
+}
+
+export default builds;
