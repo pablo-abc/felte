9fa6953f6bff5b2799b2673bac357fe7e63d89cc
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-10 05:16:52 +0100

    build(element): output modules in separate files for tree shaking

--- packages/element/rollup.config.js
+++ packages/element/rollup.config.js
@@ -4,39 +4,49 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import bundleSize from 'rollup-plugin-bundle-size';
 import { terser } from 'rollup-plugin-terser';
+import renameNodeModules from 'rollup-plugin-rename-node-modules';
 import pkg from './package.json';
 
 const prod = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      sourcemap: prod,
-      exports: 'named',
-      name: 'FelteReporterElement',
-    },
-    prod && {
-      file: 'dist/esm/index.min.js',
-      format: 'esm',
-      sourcemap: prod,
-      exports: 'named',
-      plugins: [terser()],
-    },
-    { file: pkg.module, format: 'esm', sourcemap: prod, exports: 'named' },
-  ],
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(
-        prod ? 'production' : 'development'
-      ),
-      preventAssignment: true,
-    }),
-    resolve({ browser: true, exportConditions: prod ? [] : ['development'] }),
-    commonjs(),
-    typescript({ browserslist: false }),
-    prod && bundleSize(),
-  ],
-};
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'umd',
+        sourcemap: prod,
+        exports: 'named',
+        name: 'FelteReporterElement',
+      },
+      prod && {
+        file: 'dist/esm/index.min.js',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+        plugins: [terser(), bundleSize()],
+      },
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        sourcemap: prod,
+        exports: 'named',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+      },
+    ],
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(
+          prod ? 'production' : 'development'
+        ),
+        preventAssignment: true,
+      }),
+      resolve({ browser: true, exportConditions: prod ? [] : ['development'] }),
+      commonjs(),
+      typescript({ browserslist: false }),
+      renameNodeModules('external', prod),
+    ],
+  },
+];
