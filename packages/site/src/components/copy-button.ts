350f666a57dfe89b4ff8f1b1b4e53a7614e41acf
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-22 01:35:08 +0100

    refactor(site): use Lit

--- packages/site/src/components/copy-button.ts
+++ packages/site/src/components/copy-button.ts
@@ -1,10 +1,16 @@
+import { LitElement, html, css, svg } from 'lit';
+import { customElement, state, property, query } from 'lit/decorators.js';
 import type { Instance as TippyInstance } from 'tippy.js';
 import tippy from 'tippy.js';
 
-const template = document.createElement('template');
+@customElement('copy-button')
+export class CopyButton extends LitElement {
+  static shadowRootOptions = {
+    ...LitElement.shadowRootOptions,
+    delegatesFocus: true,
+  };
 
-template.innerHTML = /* HTML */ `
-  <style>
+  static styles = css`
     :host {
       display: block;
       position: absolute;
@@ -36,91 +42,76 @@ template.innerHTML = /* HTML */ `
     button:active {
       background: rgba(255, 255, 255, 0.5);
     }
-  </style>
-  <button type="button" aria-label="Copy to clipboard"></button>
-`;
+  `;
 
-const copySvg = /* HTML */ `
-  <svg
-    aria-hidden="true"
-    fill="none"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-    ></path>
-  </svg>
-`;
-
-const copiedSvg = /* HTML */ `
-  <svg
-    aria-hidden="true"
-    fill="none"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M5 13l4 4L19 7"
-    ></path>
-  </svg>
-`;
-
-export class CopyButton extends HTMLElement {
+  @property()
   text = '';
 
-  button!: HTMLButtonElement;
-
+  @state()
   copied = false;
+
+  @query('button')
+  button!: HTMLButtonElement;
 
   instance?: TippyInstance;
 
   handleClick() {
+    // eslint-disable-next-line compat/compat
     navigator.clipboard.writeText(this.text).then(() => {
       this.copied = true;
-      this.button.innerHTML = copiedSvg;
       this.instance.setContent('Copied!');
       this.instance.show();
       setTimeout(() => {
         this.copied = false;
-        this.button.innerHTML = copySvg;
         this.instance?.setContent('Copy to clipboard');
       }, 500);
     });
   }
 
-  connectedCallback() {
-    this.attachShadow({ mode: 'open', delegatesFocus: true });
-    const content = template.content.cloneNode(true);
-    this.shadowRoot!.appendChild(content);
-    this.button = this.shadowRoot!.querySelector('button');
-    this.button.innerHTML = copySvg;
+  firstUpdated() {
     this.instance = tippy(this.button, {
       content: 'Copy to clipboard',
       placement: 'left',
     });
-    this.button.addEventListener('click', this.handleClick.bind(this));
   }
 
-  disconnectedCallback() {
-    this.button.addEventListener('click', this.handleClick.bind(this));
+  renderSvg() {
+    if (this.copied)
+      return svg/* HTML */ `
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M5 13l4 4L19 7"
+        ></path>
+      `;
+    return svg/* HTML */ `
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+      ></path>
+    `;
   }
 
-  static get observedAttributes() {
-    return ['text'];
-  }
-
-  attributeChangedCallback(name: string, _: string, newValue: string) {
-    if (name === 'text') this.text = newValue;
+  render() {
+    return html`
+      <button
+        @click=${this.handleClick}
+        type="button"
+        aria-label="Copy to clipboard"
+      >
+        <svg
+          aria-hidden="true"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          ${this.renderSvg()}
+        </svg>
+      </button>
+    `;
   }
 }
-
-customElements.define('copy-button', CopyButton);
