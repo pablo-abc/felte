c305fe65a3761658f0fe1ce3f8ca697cb6f8d58f
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-23 00:29:20 +0100

    chore: update example

--- examples/lit/basic/src/sign-in-form.ts
+++ examples/lit/basic/src/sign-in-form.ts
@@ -1,7 +1,7 @@
 import { html, LitElement, nothing } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { reporter } from '@felte/reporter-element';
-import { prepareForm } from '@felte/element';
+import '@felte/element';
 import styles from './sign-in-form.styles';
 
 type Data = {
@@ -20,35 +20,6 @@ export class SignInForm extends LitElement {
     this.submitted = undefined;
   }
 
-  /**
-   * Running `prepareForm` on `connectedCallback` allows us to guarantee
-   * we set the configuration before the form loads.
-   * `prepareForm` does nothing if the form has already loadad.
-   *
-   * If you need to change the form's configuration after load,
-   * use `HTMLFelteFormElement.configuration`.
-   */
-  connectedCallback() {
-    super.connectedCallback();
-    prepareForm<Data>('signin', {
-      onSubmit: (values) => {
-        this.submitted = values;
-      },
-      validate(values) {
-        const errors: { email: string[]; password: string[] } = {
-          email: [],
-          password: [],
-        };
-        if (!values.email) errors.email.push('Must not be empty');
-        if (!/[a-zA-Z][^@]*@[a-zA-Z][^@.]*\.[a-z]{2,}/.test(values.email))
-          errors.email.push('Must be a valid email');
-        if (!values.password) errors.password.push('Must not be empty');
-        return errors;
-      },
-      extend: [reporter],
-    });
-  }
-
   render() {
     return html`
       <h1>Basic Example - Lit</h1>
@@ -58,7 +29,26 @@ export class SignInForm extends LitElement {
           <li data-part="item"></li>
         </ul>
       </template>
-      <felte-form id="signin" @reset=${this.handleReset}>
+      <felte-form
+        @reset=${this.handleReset}
+        .configuration=${{
+          onSubmit: (values: Data) => {
+            this.submitted = values;
+          },
+          validate(values: Data) {
+            const errors: { email: string[]; password: string[] } = {
+              email: [],
+              password: [],
+            };
+            if (!values.email) errors.email.push('Must not be empty');
+            if (!/[a-zA-Z][^@]*@[a-zA-Z][^@.]*\.[a-z]{2,}/.test(values.email))
+              errors.email.push('Must be a valid email');
+            if (!values.password) errors.password.push('Must not be empty');
+            return errors;
+          },
+          extend: [reporter],
+        }}
+      >
         <form>
           <fieldset>
             <legend>Sign In</legend>
