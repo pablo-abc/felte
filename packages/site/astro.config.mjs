de7912d07ce7f29c17bdc28043ebaa11333b1c7c
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-04-05 14:37:57 +0200

    chore(site): upgrade astro

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,18 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import turbolinks from '@astrojs/turbolinks';
+import sitemap from '@astrojs/sitemap';
 
-export default {
-  projectRoot: '.', // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+export default defineConfig({
+  root: '.', // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
-  dist: './build', // When running `astro build`, path to final static output
+  outDir: './build', // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: 'https://felte.dev', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  integrations: [svelte(), turbolinks()],
-};
+  site: 'https://felte.dev', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  integrations: [svelte(), turbolinks(), sitemap()],
+});
